f6de5ef2d5d0fcd46dcbd9318ec9184d82e2138d
SergioCallanPerez <153020034+SergioCallanPerez@users.noreply.github.com>
2025-05-16 02:10:10 -0500

    Adding 1st version of grupoCursos

--- src/evaluaciones/pages/asignarformula/index.jsx
+++ src/evaluaciones/pages/asignarformula/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import GrupoCursos from "./sections/grupoCursos";
 
 export default function AsignarFormula(){
     const navigate= useNavigate();
@@ -9,6 +10,7 @@ export default function AsignarFormula(){
     return(
         <main>
             <Button onClick={()=> navigate("../")}>Ir a inicio</Button>
+            <GrupoCursos/>
         </main>
     );
 }
