978df912ac56f4f11f5bf8ffab3358f23aed6ddb
Piero Rojas <133281510+RiotRed@users.noreply.github.com>
2025-05-23 01:24:54 -0500

    grupoComponente

--- src/evaluaciones/routes/EvaluacionesRoutes.jsx
+++ src/evaluaciones/routes/EvaluacionesRoutes.jsx
@@ -3,6 +3,8 @@ import AsociarComponentesPage from "../pages/AsociarComponentesPage";
 import { CompetenciasPage } from "../pages/CompetenciasPage";
 import Formulas from "../pages/formulas";
 import AsignarFormula from "../pages/asignarformula";
+import ComponentePage from "../pages/componente"; 
+
 import MainEval from "../pages/evalHome";
 
 export const EvaluacionesRoutes = () => {
@@ -14,9 +16,11 @@ export const EvaluacionesRoutes = () => {
     <Routes>
       <Route path="componente" element={<AsociarComponentesPage />} />
       <Route path="competencias" element={<CompetenciasPage />} />
-      <Route path='formulas' element={<Formulas/>}/>
-      <Route path="asignarformula" element={<AsignarFormula/>}/>
-      <Route path="/" element={<MainEval/>}/>
+      <Route path="formulas" element={<Formulas />} />
+      <Route path="asignarformula" element={<AsignarFormula />} />
+      <Route path="componentes" element={<ComponentePage />} /> 
+      <Route path="/" element={<MainEval />} />
     </Routes>
   );
 };
+
